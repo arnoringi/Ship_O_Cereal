40802c9a98002cb38bcf5c04a84f8d022572a88d
Arcmazter <aronlex@gmail.com>
2021-05-03 11:38:35 +0000

    Initial commit

---
+++ asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Ship_O_Cereal project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ship_O_Cereal.settings')
+
+application = get_asgi_application()
